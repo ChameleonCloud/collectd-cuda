7f7f37765ffb049f1b364ff770f1cb8c0727ca28
jakecoll <jakecoll@uchicago.edu>
2018-11-15 17:41:20 -0600

    Changed metric naming convention

--- collectd_cuda.sh
+++ collectd_cuda.sh
@@ -6,8 +6,8 @@
 # This script is a collectd plugin that gathers metrics
 # from nVidia cards.
 
-HOSTNAME="${COLLECTD_HOSTNAME:-$(hostname --fqdn)}"                              
-INTERVAL="${COLLECTD_INTERVAL:-10}"         
+HOSTNAME="${COLLECTD_HOSTNAME:-$(hostname --fqdn)}"
+INTERVAL="${COLLECTD_INTERVAL:-10}"
 
 # Reading the config file.
 config_file=$(dirname "$0")/plugins_config.sh
@@ -23,7 +23,7 @@ fi
 # uniquely identify a GPU card (very useful in multiGPU
 # machines). nvidia-smi also accepts this ID as a parameter
 # in many queries. Alternatively, one can use 'uuid'.
-query_string="pci.bus_id,"
+query_string="index,"
 
 # Building the nvidia-smi --query-gpu string.
 # The last comma is removed during variable expansion
@@ -45,11 +45,9 @@ do
 	for parameter in "${!config[@]}"
 	do
 		# GPU power state, 'pstate', is returned as a number with
-		# 'P' prefix. The easiest way to remove the 'P' without 
+		# 'P' prefix. The easiest way to remove the 'P' without
 		# additional loops and conditions, is to simply expand the
 		# 'parameter' variable with '//P'.
-		echo "PUTVAL ${HOSTNAME}/cuda-${gpu_id}/${config[$parameter]}-${parameter} interval=$INTERVAL N:${!parameter//P}"
+		echo "PUTVAL ${HOSTNAME}/cuda-gpu${gpu_id}/${config[$parameter]}-${parameter} interval=$INTERVAL N:${!parameter//P}"
 	done
 done <<< "${gpus_state// }"
-
-
